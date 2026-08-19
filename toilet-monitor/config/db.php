2611d9af404a8db8230b70c430618de940951d9a
unknown <yingna0770708@gmail.com>
2026-08-19 16:28:51 +0800

    1111

--- toilet-monitor/config/db.php
+++ toilet-monitor/config/db.php
@@ -5,10 +5,10 @@
  */
 
 $DB_HOST = 'localhost';
-$DB_NAME = 'toilet_monitor';
-$DB_USER = 'root';
-$DB_PASS = '1234';
-$DB_PORT = 3307;
+$DB_NAME = 'synergy1_khoryingna_schoolnotes';
+$DB_USER = 'synergy1_yenping';
+$DB_PASS = 'R.zb0ZwEuGZ}*fW2';
+$DB_PORT = 3306;
 
 try {
     $pdo = new PDO(
